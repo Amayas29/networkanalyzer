16b44fd2f877299037866277404ac065db0aaf2d
HamidKolli <gamog5756@gmail.com>
2021-11-24 21:48:14 +0100

    add final attributs

--- src/fr/networkanalyzer/model/layers/protocols/Dns.java
+++ src/fr/networkanalyzer/model/layers/protocols/Dns.java
@@ -3,6 +3,7 @@ package fr.networkanalyzer.model.layers.protocols;
 import java.util.List;
 
 import fr.networkanalyzer.model.exceptions.NetworkAnalyzerException;
+import fr.networkanalyzer.model.fields.Entry;
 import fr.networkanalyzer.model.fields.IField;
 import fr.networkanalyzer.model.layers.AbstractLayer;
 import fr.networkanalyzer.model.layers.ILayerApplication;
@@ -10,6 +11,20 @@ import fr.networkanalyzer.model.visitors.ILayerVisitor;
 
 public class Dns extends AbstractLayer implements ILayerApplication {
 
+	
+	public static final Entry IDENTIFIER = new Entry("Identification",16);
+	public static final Entry FLAGS = new Entry("Flags",16);
+	public static final Entry QUESTIONS_NUMBER = new Entry("Questions number",16);
+	public static final Entry ANSWER_RRS_NUMBER = new Entry("# Answer RRS",16);
+	public static final Entry AUTHORITY_RRS_NUMBER = new Entry("# Authority RRs",16);
+	public static final Entry ADDITIONAL_RRS_NUMBER = new Entry("# Additional RRs",16);
+	public static final Entry QUESTIONS = new Entry("Questions",32);
+	public static final Entry ANSWER = new Entry("Answer",32);
+	public static final Entry AUTHORITY = new Entry("Authority",32);
+	public static final Entry ADDITIONAL_INFO = new Entry("Additional info",32);
+
+
+	
 	@Override
 	public void accept(ILayerVisitor visitor) throws NetworkAnalyzerException {
 		visitor.visit(this);
--- src/fr/networkanalyzer/model/layers/protocols/Icmp.java
+++ src/fr/networkanalyzer/model/layers/protocols/Icmp.java
@@ -3,6 +3,7 @@ package fr.networkanalyzer.model.layers.protocols;
 import java.util.List;
 
 import fr.networkanalyzer.model.exceptions.NetworkAnalyzerException;
+import fr.networkanalyzer.model.fields.Entry;
 import fr.networkanalyzer.model.fields.IField;
 import fr.networkanalyzer.model.layers.AbstractLayer;
 import fr.networkanalyzer.model.layers.ILayerTransport;
@@ -10,6 +11,11 @@ import fr.networkanalyzer.model.visitors.ILayerVisitor;
 
 public class Icmp extends AbstractLayer implements ILayerTransport {
 
+	public static final Entry TYPE = new Entry("Type",8);
+	public static final Entry Code = new Entry("Code",8);
+	public static final Entry CHECKSUM = new Entry("Checksum",16);
+	public static final Entry IDENTIFIER = new Entry("Identifier",16);
+	public static final Entry SEQUENCE_NUMBER = new Entry("Sequence number",8);
 	@Override
 	public void accept(ILayerVisitor visitor) throws NetworkAnalyzerException {
 		visitor.visit(this);
