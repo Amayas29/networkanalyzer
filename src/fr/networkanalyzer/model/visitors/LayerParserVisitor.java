689e01c8e2f8c69e7ad04d355919e040e522f7fb
Amayas29 <amayas29sd@gmail.com>
2021-11-24 10:57:12 +0100

    parser udp

--- src/fr/networkanalyzer/model/visitors/LayerParserVisitor.java
+++ src/fr/networkanalyzer/model/visitors/LayerParserVisitor.java
@@ -91,10 +91,14 @@ public class LayerParserVisitor implements ILayerVisitor {
 
 		String header = getHeader(42).trim();
 
-		String destMacAddress = header.substring(0, 17);
-		String srcMacAddress = header.substring(18, 35);
-		String rdType = header.substring(36);
+		System.out.println("Header ethernet : *" + header + "*");
 
+		String destMacAddress = header.substring(0, 17);
+		System.out.println("Mac dest : *" + destMacAddress + "*");
+		String srcMacAddress = header.substring(18, 35);
+		System.out.println("Mac src : *" + srcMacAddress + "*");
+		String rdType = header.substring(36);
+		System.out.println("type : *" + rdType + "*");
 
 		IField type;
 		ILayerNetwork layer = null;
@@ -161,50 +165,58 @@ public class LayerParserVisitor implements ILayerVisitor {
 	@Override
 	public void visit(Ip ip) throws NetworkAnalyzerException {
 
-		String header = getHeader(60);
-		System.out.println("*"+header);
-		String version = header.substring(0,1);
-		System.out.println(version);
+		String header = getHeader(60).trim();
+		System.out.println("Header ip : *" + header + "*");
+
+		String version = header.substring(0, 1);
+		System.out.println("Version : *" + version + "*");
 		if (!version.equals("4"))
 			throw new NetworkanalyzerParseErrorException(getLine(), "The IP Vesion is not compatible");
 
 		currentIndex += 1;
 
-		String ihl = header.substring(1,2);
+		String ihl = header.substring(1, 2);
+		System.out.println("Ihl : *" + ihl + "*");
 		currentIndex += 1;
 		if (Integer.parseInt(ihl, 16) < 5)
 			throw new NetworkanalyzerParseErrorException(getLine(), "The IP IHL is not compatible");
 
-		String tos = header.substring(3,5);
-		String totalLength = header.substring(6,11);
-		String identification = header.substring(12,17);
-		String fr = Integer.toBinaryString(Integer.parseInt(header.substring(18,23).replace(" ","")));
-		
-		while(fr.length() != 16)
+		String tos = header.substring(3, 5);
+		System.out.println("Tos : *" + tos + "*");
+		String totalLength = header.substring(6, 11);
+		System.out.println("Total len : *" + totalLength + "*");
+		String identification = header.substring(12, 17);
+		System.out.println("Identific : *" + identification + "*");
+		String fr = Integer.toBinaryString(Integer.parseInt(header.substring(18, 23).replace(" ", "")));
+		System.out.println("Frags : *" + fr + "*");
+		while (fr.length() != 16)
 			fr = "0".concat(fr);
-		
-		String r = fr.substring(0,1);
-		String df = fr.substring(1,2);
-		String mf = fr.substring(2,3);
+
+		String r = fr.substring(0, 1);
+		String df = fr.substring(1, 2);
+		String mf = fr.substring(2, 3);
 		String fragmentOffset = fr.substring(3);
-		String ttl = header.substring(24,26);
-		String protocol = header.substring(27,29);
+
+		String ttl = header.substring(24, 26);
+		System.out.println("Ttl : *" + ttl + "*");
+		String protocol = header.substring(27, 29);
+		System.out.println("protocol : *" + protocol + "*");
 		currentIndex += 25;
-		ILayerTransport transport;
+		ILayerTransport layer;
 		IField proto;
 		switch (Integer.parseInt(protocol, 16)) {
 		case Ip.ICMP: {
-			transport = new Icmp();
+			layer = new Icmp();
 			proto = new Field(Ip.PROTOCOL, protocol, "ICMP");
 			break;
 		}
 		case Ip.UDP: {
-			transport = new Udp();
+			layer = new Udp();
 			proto = new Field(Ip.PROTOCOL, protocol, "UDP");
 			break;
 		}
 		case Ip.TCP: {
-			transport = new Tcp();
+			layer = new Tcp();
 			proto = new Field(Ip.PROTOCOL, protocol, "TCP");
 			break;
 		}
@@ -212,11 +224,14 @@ public class LayerParserVisitor implements ILayerVisitor {
 			throw new NetworkanalyzerParseErrorException(getLine(), "Unexpected value of the IP protocol field");
 		}
 
-		String headerChecksum = header.substring(30,36);
+		String headerChecksum = header.substring(30, 35);
+		System.out.println("checksum : *" + headerChecksum + "*");
 		currentIndex += 9;
 
-		String srcAddress = header.substring(36,47);
-		String destAddress = header.substring(48,60);
+		String srcAddress = header.substring(36, 47);
+		System.out.println("src ip addr : *" + srcAddress + "*");
+		String destAddress = header.substring(48);
+		System.out.println("dest ip addr : *" + destAddress + "*");
 
 		if (srcAddress.equals("FF FF FF FF"))
 			throw new NetworkanalyzerParseErrorException(getLine(),
@@ -234,26 +249,26 @@ public class LayerParserVisitor implements ILayerVisitor {
 		ip.addField(Ip.VERSION.NAME, new Field(Ip.VERSION, version, "Ipv4"));
 		ip.addField(Ip.IHL.NAME, new Field(Ip.IHL, ihl, String.valueOf(Integer.parseInt(ihl.replace(" ", ""), 16))));
 		ip.addField(Ip.TOS.NAME, new Field(Ip.TOS, tos, tos));
-		ip.addField(Ip.TOTAL_LENGTH.NAME,
-				new Field(Ip.TOTAL_LENGTH, totalLength, String.valueOf(Integer.parseInt(totalLength.replace(" ", ""), 16))));
+		ip.addField(Ip.TOTAL_LENGTH.NAME, new Field(Ip.TOTAL_LENGTH, totalLength,
+				String.valueOf(Integer.parseInt(totalLength.replace(" ", ""), 16))));
 
-		ip.addField(Ip.IDENTIFICATION.NAME,
-				new Field(Ip.IDENTIFICATION, identification, String.valueOf(Integer.parseInt(identification.replace(" ", ""), 16))));
+		ip.addField(Ip.IDENTIFICATION.NAME, new Field(Ip.IDENTIFICATION, identification,
+				String.valueOf(Integer.parseInt(identification.replace(" ", ""), 16))));
 
 		Fields fragments = new Fields(Ip.FRAGMENTS.NAME);
 		fragments.addField(new Field(Ip.R, r, r));
 		fragments.addField(new Field(Ip.DF, df, df));
 		fragments.addField(new Field(Ip.MF, mf, mf));
-		fragments.addField(
-				new Field(Ip.FRAGMENT_OFFSET, fragmentOffset, String.valueOf(Integer.parseInt(fragmentOffset, 16))));
+		fragments.addField(new Field(Ip.FRAGMENT_OFFSET, fragmentOffset,
+				String.valueOf(Integer.parseInt(fragmentOffset.replace(" ", ""), 2))));
 
 		ip.addField(Ip.FRAGMENTS.NAME, fragments);
 
 		ip.addField(Ip.TTL.NAME, new Field(Ip.TTL, ttl, String.valueOf(Integer.parseInt(ttl.replace(" ", ""), 16))));
 		ip.addField(Ip.HEADER_CHECKSUM.NAME, new Field(Ip.HEADER_CHECKSUM, headerChecksum, headerChecksum));
-		transport = new Udp();
-		transport.accept(this);
-		ip.setIncluded(transport);
+		layer = new Udp();
+		layer.accept(this);
+		ip.setIncluded(layer);
 	}
 
 	@Override
@@ -263,15 +278,54 @@ public class LayerParserVisitor implements ILayerVisitor {
 
 	@Override
 	public void visit(Udp udp) throws NetworkAnalyzerException {
-//		String header = getHeader(0, 41);
-		udp.addField(Udp.SRC_PORT.NAME, new Field(Udp.SRC_PORT, "00 80", "80"));
-		udp.addField(Udp.DEST_PORT.NAME, new Field(Udp.DEST_PORT, "00 80", "80"));
-		udp.addField(Udp.LENGTH.NAME, new Field(Udp.LENGTH, "00 80", "80"));
-		udp.addField(Udp.CHECKSUM.NAME, new Field(Udp.CHECKSUM, "00 80", "80"));
 
-		ILayerApplication dhcp = new Dhcp();
-		dhcp.accept(this);
-		udp.setIncluded(dhcp);
+		ILayerApplication layer;
+
+		String header = getHeader(24).trim();
+		System.out.println("Header udp : *" + header + "*");
+
+		String srcPort = header.substring(0, 5);
+
+		System.out.println("srcPort : *" + srcPort + "*");
+
+		String destPort = header.substring(6, 11);
+
+		System.out.println("destPort : *" + destPort + "*");
+
+		int pDest = Integer.parseInt(destPort.replace(" ", ""), 16);
+
+		switch (pDest) {
+		case Udp.DNS: {
+			layer = new Dns();
+			break;
+		}
+
+		case Udp.DHCP: {
+			layer = new Dhcp();
+			break;
+		}
+		default:
+			throw new NetworkanalyzerParseErrorException(getLine(),
+					"Unexpected value of the Udp port destination field");
+		}
+
+		String length = header.substring(12, 17);
+
+		System.out.println("udp length : *" + length + "*");
+		String checksum = header.substring(18);
+
+		System.out.println("udp checksum : *" + checksum + "*");
+
+		udp.addField(Udp.SRC_PORT.NAME,
+				new Field(Udp.SRC_PORT, srcPort, String.valueOf(Integer.parseInt(srcPort.replace(" ", ""), 16))));
+		udp.addField(Udp.DEST_PORT.NAME, new Field(Udp.DEST_PORT, destPort, String.valueOf(pDest)));
+		udp.addField(Udp.LENGTH.NAME,
+				new Field(Udp.LENGTH, length, String.valueOf(Integer.parseInt(length.replace(" ", ""), 16))));
+		udp.addField(Udp.CHECKSUM.NAME,
+				new Field(Udp.CHECKSUM, checksum, String.valueOf(Integer.parseInt(checksum.replace(" ", ""), 16))));
+
+//		layer.accept(this);
+		udp.setIncluded(layer);
 	}
 
 	private int getLine() {
