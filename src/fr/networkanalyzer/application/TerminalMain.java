7ef86db3c0c1abb0840a0adeb75383a918e7cb08
HamidKolli <gamog5756@gmail.com>
2021-12-08 02:26:08 +0100

    terminal

--- src/fr/networkanalyzer/application/TerminalMain.java
+++ src/fr/networkanalyzer/application/TerminalMain.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import fr.networkanalyzer.model.Analyzer;
 import fr.networkanalyzer.model.AnalyzerParser;
+import fr.networkanalyzer.model.Frame;
 import fr.networkanalyzer.model.exceptions.NetworkAnalyzerException;
 
 public class TerminalMain {
@@ -36,10 +37,12 @@ public class TerminalMain {
 
 		}
 
-		displayMain();
-
 		while (true) {
+			System.out.println("***Please press ENTER to continue***");
+		
+			sc.nextLine();
 			System.out.println("\tplease choose one of this numbers : ");
+			displayMain();
 			int choose;
 			try {
 				choose = sc.nextInt();
@@ -50,20 +53,72 @@ public class TerminalMain {
 			switch (choose) {
 			case 0: {
 				System.out.println(analyzer);
+				sc.nextLine();
 				break;
 			}
 			case 1: {
+				System.out.println("\t\t please enter the id frame");
+				try {
+					int c = sc.nextInt();
+					boolean b = true;
+					for (Frame f : analyzer.getFrames()) {
+						if (f.getId() == c) {
+							System.out.println(f);
+							b = false;
+							break;
+
+						}
+					}
+					if (b) {
+						System.out.println("\t\t the frame doesn't exist");
+						
+					}
+
+				} catch (Exception e) {
+					System.out.println("\t\t invalid number \n");
+				}
+				sc.nextLine();
+				continue;
+
+			}
+			case 2: {
 				try {
 					AnalyzerParser.saveAll(analyzer);
 				} catch (NetworkAnalyzerException e) {
-					continue;
+					
 				}
-				break;
+				
+				continue;
 			}
-			case 2: {
+			case 3: {
+				System.out.println("\t\t please enter the id frame");
+				try {
+					int c = sc.nextInt();
+					boolean b = true;
+					for (Frame f : analyzer.getFrames()) {
+						if (f.getId() == c) {
+							AnalyzerParser.save(f);
+							b = false;
+							break;
+
+						}
+					}
+					if (b) {
+						System.out.println("\t\t the frame doesn't exist");
+						
+					}
+
+				} catch (Exception e) {
+					System.out.println("\t\t invalid number \n");
+				}
+				sc.nextLine();
+				continue;
+			}
+			case 4: {
 				System.out.println("\t Good bye");
 				System.exit(0);
-				break;
+				
+				continue;
 			}
 			default:
 				continue;
@@ -77,8 +132,10 @@ public class TerminalMain {
 
 		System.out.println("\t+-------------------+");
 		System.out.println("\t| 0- display frames |");
-		System.out.println("\t| 1- save frames    |");
-		System.out.println("\t| 2- exit           |");
+		System.out.println("\t| 1- display frame  |");
+		System.out.println("\t| 2- save frames    |");
+		System.out.println("\t| 3- save frame     |");
+		System.out.println("\t| 4- exit           |");
 		System.out.println("\t+-------------------+");
 
 	}
